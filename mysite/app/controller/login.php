2206ba8c3b7c9b4b731bc581a02881b9e4e95c73
Nic-14-test <153505295+Nic-14-test@users.noreply.github.com>
2023-12-11 18:15:19 +0800

    Add files via upload

---
+++ mysite/app/controller/login.php
@@ -0,0 +1,10 @@
+<?php
+
+Class Login extends Controller
+{
+
+    public function index()
+    {
+        $this->view("sit/login");
+    }
+}
---
+++ mysite/app/controller/signup.php
@@ -0,0 +1,10 @@
+<?php
+
+Class Signup extends Controller
+{
+
+    public function index()
+    {
+        $this->view("sit/signup");
+    }
+}
